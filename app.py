03f97f3408ee0a681e5ce01dd4830d5083c99e05
200IQLulu <ludvigmegeberg@gmail.com>
2025-10-10 13:56:01 +0200

    Jeg lagde linker og litt syle til nettsiden

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def om():
+    return render_template("about.html")
+
+@app.route('/base')
+def hoved():
+    return render_template("base.html")
+
+@app.route('/contact')
+def kontakt():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
