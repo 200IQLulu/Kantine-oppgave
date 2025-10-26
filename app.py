2cdea91c015249a9a7d5a9d44937f9796e5866a4
200IQLulu <ludvigmegeberg@gmail.com>
2025-10-26 22:58:02 +0100

    jeg har lagt til beskrivelse til produktene og til menyen

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ def index():
 def om():
     # Statisk side med faste varer og priser
     omen = [
-        { "produkt": "Brødskive med ost", "pris": "25 kr", "bilde": "/static/images/ost.webp"},
-        { "produkt": "Brødskive med skinke", "pris": "25 kr", "bilde": "/static/images/skinke.jpg"},
-        { "produkt": "Fruktbeger", "pris": "20 kr", "bilde": "/static/images/fruktbeger.jpg"},
-        { "produkt": "Kaffe", "pris": "15 kr", "bilde": "/static/images/kaffe.webp"},
-        { "produkt": "Juice", "pris": "20 kr", "bilde": "/static/images/juice.webp"},
+        { "produkt": "Brødskive med ost", "pris": "25 kr", "bilde": "/static/images/ost.webp", "beskrivelse": "Fersk brødskive med ost"},
+        { "produkt": "Brødskive med skinke", "pris": "25 kr", "bilde": "/static/images/skinke.jpg", "beskrivelse": "Fersk brødskive med skinke"},
+        { "produkt": "Fruktbeger", "pris": "20 kr", "bilde": "/static/images/fruktbeger.jpg", "beskrivelse": "Blandet frukt i et beger"},
+        { "produkt": "Kaffe", "pris": "15 kr", "bilde": "/static/images/kaffe.webp", "beskrivelse": "Fersk brygget kaffe"},
+        { "produkt": "Juice", "pris": "20 kr", "bilde": "/static/images/juice.webp", "beskrivelse": "Fruktjuice i forskjellige smaker"},
     ]
     return render_template("about.html", omen=omen)
 
@@ -32,11 +32,11 @@ def kontakt():
 def ukesmeny():
     # Data som sendes inn i Jinja2-malen menu.html
     weekly_menu = [
-        {"dag": "Mandag", "rett": "Kyllingsalat", "pris": "49 kr", "bilde": "/static/images/Kylling.jpg"},
-        {"dag": "Tirsdag", "rett": "Pasta Bolognese", "pris": "59 kr", "bilde": "/static/images/pasta.gif"},
-        {"dag": "Onsdag", "rett": "Vegetar wok", "pris": "45 kr", "bilde": "/static/images/wok.avif"},
-        {"dag": "Torsdag", "rett": "Fiskeboller i hvit saus", "pris": "65 kr", "bilde": "/static/images/fiskeboller.jpeg"},
-        {"dag": "Fredag", "rett": "Taco buffet", "pris": "79 kr", "bilde": "/static/images/taco.jpeg"},
+        {"dag": "Mandag", "rett": "Kyllingsalat", "pris": "49 kr", "bilde": "/static/images/kylling.jpg", "beskrivelse": "Frisk salat med kyllingkjøtt og grønnsaker"},
+        {"dag": "Tirsdag", "rett": "Pasta Bolognese", "pris": "59 kr", "bilde": "/static/images/pasta.gif", "beskrivelse": "Klassisk italiensk pasta med kjøttsaus"},
+        {"dag": "Onsdag", "rett": "Vegetar wok", "pris": "45 kr", "bilde": "/static/images/wok.avif", "beskrivelse": "Woket grønnsaker med asiatisk smak"},
+        {"dag": "Torsdag", "rett": "Fiskeboller i hvit saus", "pris": "65 kr", "bilde": "/static/images/fiskeboller.jpeg", "beskrivelse": "Tradisjonelle norske fiskeboller"},
+        {"dag": "Fredag", "rett": "Taco buffet", "pris": "79 kr", "bilde": "/static/images/taco.jpeg", "beskrivelse": "Mexicansk inspirert taco med diverse pålegg"},
     ]
     return render_template("menu.html", weekly_menu=weekly_menu)
 
